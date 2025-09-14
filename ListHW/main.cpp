f3431474f252894eeb1fa644cf729cff550d57a9
un <un@DESKTOP-JAU235M>
2025-09-14 23:22:31 +0300

    list 2 in progress

---
+++ ListHW/main.cpp
@@ -0,0 +1,216 @@
+﻿#include <iostream>
+#include <ctime>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define delimiter "\n--------------------------------------------------------\n"
+#define tab "\t"
+//#define DEBUG
+
+class List
+{
+	class Element
+	{
+		int Data;
+		Element* pNext;
+		Element* pPrev;
+	public:
+		Element(int Data, Element* pNext = nullptr, Element* pPrev = nullptr)
+			:Data(Data), pNext(pNext), pPrev(pPrev)
+		{
+			cout << "EConstructor:\t" << this << endl;
+		}
+		~Element()
+		{
+			cout << "EDestructor:\t" << this << endl;
+		}
+		friend class List;
+	}*Head, *Tail;
+	//Объекты классов и структур и указатели на эти объекты можно обяъвлять непосредственно после описания классов и структур
+	size_t size;
+public:
+	List()
+	{
+		Head = Tail = nullptr;
+		size = 0;
+		cout << "LConstructor:\t" << this << endl;
+	}
+	~List()
+	{
+		//while (Head)pop_front();
+		while (Tail)pop_back();
+		cout << "LDestructor:\t" << this << endl;
+	}
+
+	//					Adding Elements:
+	void push_front(int Data)
+	{
+		//1) Создаём элемент, в которм будет храниться добавляемое значение:
+		Element* New = new Element(Data);
+		if (Head == nullptr && Tail == nullptr)
+		{
+			Head = Tail = New;
+		}
+		else
+		{
+			//2) Пристыковываем новый элемент к списку:
+			New->pNext = Head;
+
+			//3) Пристыковываем начальный элемент списка к новому элементу
+			Head->pPrev = New;
+
+			//4) Делаем новый элемент начальным элементом списка:
+			Head = New;
+		}
+		size++;
+	}
+	void push_back(int Data)
+	{
+		//1) Создаём новый элемент
+		Element* New = new Element(Data);
+		if (Head == nullptr && Tail == nullptr)
+		{
+			Head = Tail = New;
+		}
+		else
+		{
+			//2) Цепляем новый элемент за Хвост списка:
+			New->pPrev = Tail;
+
+			//3)Пристыковываем список к новому элементу:
+			Tail->pNext = New;
+
+			//4) Делаем новый элемент хвостом списка:
+			Tail = New;
+		}
+		size++;
+	}
+	void insert(int Data, int Index)
+	{
+		if (Index < 0)return;
+		if (Index == 0 || size == 0)return push_front(Data);
+		if (Index >= size)return push_back(Data);
+		//1) Доходим до нужного элемента:
+		Element* Temp;
+		if (Index > size / 2)
+		{
+			Temp = Head;
+			for (int i = 0; i < Index; i++)Temp = Temp->pNext;
+		}
+		else
+		{
+			Temp = Tail;
+			for (int i = 0; i < size - Index - 1; i++)Temp = Temp->pPrev;
+		}
+		// Независимо от того, каким образом мы дошли до нужного элемента, с начала списка или с конца списка
+		// процедура добавления будет идентичной:
+		
+		//2) Создаём новый элемент
+		Element* New = new Element(Data);
+
+		//3) Пристыковываем новый элемент к списку
+		New->pNext = Temp;
+		New->pPrev = Temp->pPrev;
+
+		//4) Вклиниваем новый элемент в список
+		Temp->pPrev->pNext = New;
+		Temp->pPrev = New;
+
+		size++;
+	}
+
+	//Removing elements:
+	void pop_front()
+	{
+		if (Head == nullptr && Tail == nullptr)return;
+		if (Head == Tail)
+		{
+			delete Head;
+			Head = Tail = nullptr;
+		}
+		else
+		{
+			//1) Смещаем голову на следующий элемент:
+			Head = Head->pNext;
+
+			//2) Удаляем элемент из памяти:
+			delete Head->pPrev;
+
+			//3) Обнуляем указатель на удалённый элемент (делаем Head последним элементом списка)
+			Head->pPrev = nullptr;
+		}
+		size--;
+	}
+	void pop_back()
+	{
+		if (Head == nullptr && Tail == nullptr)return;
+		if (Head == Tail)
+		{
+			delete Tail;
+			Head = Tail = nullptr;
+		}
+		else
+		{
+			Tail = Tail->pPrev;
+			delete Tail->pNext;
+			Tail->pNext = nullptr;
+		}
+		size--;
+	}
+
+	//					Methods:
+	void print() const
+	{
+		cout << delimiter;
+		cout << "Head: " << Head << endl;
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Tail: " << Tail << endl;
+		cout << "Количество элементов списка: " << size;
+		cout << delimiter;
+	}
+	void reverse_print() const
+	{
+		cout << delimiter << endl;
+		cout << "Tail: " << Tail << endl;
+		for(Element* Temp = Tail; Temp; Temp = Temp->pPrev)
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Head: " << Head << endl;
+		cout << "Количество элементов списка: " << size;
+		cout << delimiter << endl;
+	}
+};
+
+//#define BASE_CHECK
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef BASE_CHECK
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+	List list;
+	for (int i = 0; i < n; i++)
+	{
+		//list.push_front(rand() % 100);
+		list.push_back(rand() % 100);
+	}
+	list.print();
+	list.reverse_print();
+	/*list.pop_back();
+	list.print();
+	list.reverse_print();*/
+
+	int index;
+	int value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	list.insert(value, index);
+	list.print();
+	list.reverse_print();
+#endif // BASE_CHECK
+
+
+}
