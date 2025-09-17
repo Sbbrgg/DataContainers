389cf6c25c366ad7fca6942f7f508abb51d9ab51
un <un@DESKTOP-JAU235M>
2025-09-17 14:14:09 +0300

    Inherit iterators DONE

--- ListHW/main.cpp
+++ ListHW/main.cpp
@@ -29,18 +29,182 @@ class List
 	}*Head, *Tail;
 	//Объекты классов и структур и указатели на эти объекты можно обяъвлять непосредственно после описания классов и структур
 	size_t size;
+	class ConstBaseIterator
+	{
+	protected:
+		Element* Temp;
+	public:
+		ConstBaseIterator(Element* Temp = nullptr) :Temp(Temp) {}
+		~ConstBaseIterator() {}
+
+		//			Comparison operators:
+		bool operator==(const ConstBaseIterator& other)const
+		{
+			return this->Temp == other.Temp;
+		}
+		bool operator!=(const ConstBaseIterator& other)const
+		{
+			return this->Temp != other.Temp;
+		}
+
+		int operator*()const
+		{
+			return Temp->Data;
+		}
+	};
 public:
+	class ConstIterator: public ConstBaseIterator
+	{
+	public:
+		ConstIterator(Element* Temp = nullptr) : ConstBaseIterator(Temp) {}
+		~ConstIterator() {}
+
+		ConstIterator operator++()
+		{
+			Temp = Temp->pNext;
+			return *this;
+		}
+		ConstIterator operator++(int)
+		{
+			ConstIterator old = *this;
+			Temp = Temp->pNext;
+			return old;
+		}
+		ConstIterator& operator--()
+		{
+			Temp = Temp->pPrev;
+			return *this;
+		}
+		ConstIterator& operator--(int)
+		{
+			ConstIterator old = *this;
+			Temp = Temp->pPrev;
+			return old;
+		}
+
+	};
+	class ConstReverseIterator: public ConstBaseIterator
+	{
+	public:
+		ConstReverseIterator(Element* Temp = nullptr) : ConstBaseIterator(Temp) {}
+		~ConstReverseIterator() {}
+		ConstReverseIterator& operator++()
+		{
+			Temp = Temp->pPrev;
+			return *this;
+		}
+		ConstReverseIterator operator++(int)
+		{
+			ConstReverseIterator old = *this;
+			Temp = Temp->pPrev;
+			return old;
+		}
+		ConstReverseIterator& operator--()
+		{
+			Temp = Temp->pNext;
+			return *this;
+		}
+		ConstReverseIterator operator--(int)
+		{
+			ConstReverseIterator old = *this;
+			Temp = Temp->pNext;
+			return old;
+		}
+
+		//				Comparison operators
+		
+
+	};
+	class Iterator : public ConstIterator
+	{
+	public:
+		Iterator(Element* Temp): ConstIterator(Temp){}
+		~Iterator(){}
+		int& operator* ()
+		{
+			return Temp->Data;
+		}
+	};
+	class ReverseIterator:public ConstReverseIterator
+	{
+	public:
+		ReverseIterator(Element* Temp) : ConstReverseIterator(Temp) {}
+		~ReverseIterator(){}
+		int& operator*()
+		{
+			return Temp->Data;
+		}
+	};
+
+	ConstIterator begin()const
+	{
+		return Head;
+	}
+	ConstIterator end()const
+	{
+		return nullptr;
+	}
+	ConstReverseIterator rbegin() const
+	{
+		return Tail;
+	}
+	ConstReverseIterator rend() const
+	{
+		return nullptr;
+	}
+
+	Iterator begin()
+	{
+		return Head;
+	}
+	Iterator end()
+	{
+		return nullptr;
+	}
+	ReverseIterator rbegin()
+	{
+		return Tail;
+	}
+	ReverseIterator rend()
+	{
+		return nullptr;
+	}
+
 	List()
 	{
 		Head = Tail = nullptr;
 		size = 0;
 		cout << "LConstructor:\t" << this << endl;
 	}
+	List(const std::initializer_list<int>& il) : List()
+	{
+		for (int const* it = il.begin(); it != il.end(); ++it)
+		{
+			push_back(*it);
+		}
+		cout << "ILConstructor:\t" << this << endl;
+	}
+	List(const List& other) :List()
+	{
+		*this = other;
+		cout << "LCopyConstructor:\t" << this << endl;
+	}
 	~List()
 	{
 		//while (Head)pop_front();
 		while (Tail)pop_back();
 		cout << "LDestructor:\t" << this << endl;
+	}
+
+	//					Operators:
+	List& operator=(const List& other)
+	{
+		if (this == &other) return *this;
+		while (Head)pop_front();
+		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)
+			push_back(Temp->Data);
+		cout << "LCopyAssignment:\t" << this << endl;
+		return *this;
 	}
 
 	//					Adding Elements:
@@ -182,7 +346,18 @@ public:
 	}
 };
 
+List operator+(const List& left, const List& right)
+{
+	List fusion = left;
+	for (List::ConstIterator it = right.begin(); it != right.end(); ++it)
+	{
+		fusion.push_back(*it);
+	}
+	return fusion;
+}
+
 //#define BASE_CHECK
+#define HOME_WORK
 
 void main()
 {
@@ -212,5 +387,28 @@ void main()
 	list.reverse_print();
 #endif // BASE_CHECK
 
+#ifdef HOME_WORK 
+	List list1 = { 3, 5, 8, 13, 21 };
+	List list2 = { 34, 55, 89 };
+	//list1.print();
+	//list2.print();
+	//List list3 = list1 + list2;
+	for (int i : list1)cout << i << tab; cout << endl;
+	for (int i : list2)cout << i << tab; cout << endl;
+	//for (int i : list3)cout << i << tab; cout << endl;
+
+	for (List::ConstIterator it = list1.begin(); it != list1.end(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+	for (List::Iterator it = list1.begin(); it != list1.end(); ++it)
+	{
+		*it *= 100;
+		cout << *it << tab;
+	}
+	cout << endl;
+	
+#endif // HOME_WORK 
 
 }
